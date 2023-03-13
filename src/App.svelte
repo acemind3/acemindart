362f9394673f79620abb518908677fbd704acf39
Alexia Doane <alex.doane3@gmail.com>
2023-03-12 20:26:58 -0500

    add under construction page

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,18 @@
 
   import HomePage from "./pages/home/HomePage.svelte";
   import AdoraPage from "./pages/adora/AdoraPage.svelte";
+  import UnderConstruction from "./pages/under-construction/UnderConstruction.svelte";
 
   export let url = window.location.pathname;
+
+  const isProduction = process.env.NODE_ENV == "production";
 </script>
 
-<Router basepath="" {url}>
-  <Route path="/adora" component={AdoraPage} />
-  <Route path="/"><HomePage /></Route>
-</Router>
+{#if isProduction}
+  <UnderConstruction />
+{:else}
+  <Router basepath="" {url}>
+    <Route path="/adora" component={AdoraPage} />
+    <Route path="/"><HomePage /></Route>
+  </Router>
+{/if}
---
+++ src/pages/under-construction/UnderConstruction.svelte
@@ -0,0 +1,21 @@
+<div class="under-construction">
+  <h1 class="under-construction__header">Site Under Construction</h1>
+</div>
+
+<style lang="scss">
+  .under-construction {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: #323232;
+
+    &__header {
+      font-size: 12rem;
+      text-align: center;
+      color: #fafafa;
+    }
+  }
+</style>
